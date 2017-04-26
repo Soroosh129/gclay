d18140a9d8939dd0f69d1f66e4a8d2a7ab769666
Soroosh129 <soroosh129@gmail.com>
2017-04-26 12:12:49 -0500

    Cleanup - Removed release dir

--- release/mod/CMakeFiles/gdev_module.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/gdev_module"
-  "gdev/gdev.ko"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/gdev_module.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
